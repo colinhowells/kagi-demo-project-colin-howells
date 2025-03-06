0c92e6f8dcecc213395417093b2a6e7df07c9c74
Colin Howells <colin@colinhowells.com>
2025-03-06 09:30:29 -0800

    correcting legal nomenclature

--- js/02-js-optimization.js
+++ js/02-js-optimization.js
@@ -1,16 +1,16 @@
 const HEARING_MINUTES = 30;
 
 /**
- * The time for the people before plaintiff to be heard, added to the plaintiff's own hearing time, eg 'How long will it take for your hearing to end?'
+ * The time for the people before defendant to be heard, added to the defendant's own hearing time, eg 'How long will it take for your hearing to end?'
  *
  * Typical execution times:
  * 0.10000002384185791ms
  * 0.10000002384185791ms
  * 0.09999990463256836ms
  *
- * @param {string} plaintiff first name
+ * @param {string} defendant first name
  * @param {number} numberJudges all available judges, assumed free
- * @param {string} otherPlaintiffs string of first names separated by spaces
+ * @param {string} otherDefendants string of first names separated by spaces
  * @returns {number} minutes
  * @example
  * court('Jules', 3, 'Adam Betty Frank Mike'); => 60
@@ -18,20 +18,20 @@ const HEARING_MINUTES = 30;
  * court('Abraxas', 1, 'Aaron Alexis Aphasia Aloysius'); => 60
  * court('Mickey', 2, 'Bobby Phil Jerry Billy'); => 30
  */
-function court(plaintiff, numberJudges, otherPlaintiffs) {
-	const plaintiffsAlphabetized = [plaintiff, ...otherPlaintiffs.split(' ')].sort();
+function court(defendant, numberJudges, otherDefendants) {
+	const defendantsAlphabetized = [defendant, ...otherDefendants.split(' ')].sort();
 
-	const numberOthersBeforePlaintiff = plaintiffsAlphabetized.slice(
+	const numberOthersBeforeBefendant = defendantsAlphabetized.slice(
 		0, // beginning, natch
-		plaintiffsAlphabetized.findIndex((p) => plaintiff === p) // index of plaintiff
+		defendantsAlphabetized.findIndex((p) => defendant === p) // index of defendant
 	).length;
 
-	// there is a judge immediately available for the plaintiff
-	if (numberJudges > numberOthersBeforePlaintiff) return HEARING_MINUTES;
+	// there is a judge immediately available for the defendant
+	if (numberJudges > numberOthersBeforeBefendant) return HEARING_MINUTES;
 
-	// the plaintiff is the first person to be heard after the others
-	const beforePlaintiffMinutes = (numberOthersBeforePlaintiff / numberJudges) * HEARING_MINUTES;
-	return beforePlaintiffMinutes + HEARING_MINUTES;
+	// the defendant is the first person to be heard after the others
+	const beforeDefendantMinutes = (numberOthersBeforeBefendant / numberJudges) * HEARING_MINUTES;
+	return beforeDefendantMinutes + HEARING_MINUTES;
 }
 
 /**
@@ -44,15 +44,15 @@ function handleSubmit(e) {
 
 	const formData = new FormData(e.target);
 	// in a non-demo context we would be validating/sanitizing all this
-	const plaintiff = formData.get('plaintiff').trim();
+	const defendant = formData.get('defendant').trim();
 	const numberJudges = formData.get('numberJudges');
-	const otherPlaintiffs = formData.get('otherPlaintiffs'); // trimmed via pattern enforcement
+	const otherDefendants = formData.get('otherDefendants'); // trimmed via pattern enforcement
 
-	const hearingConclusionTimeMinutes = court(plaintiff, numberJudges, otherPlaintiffs);
+	const hearingConclusionTimeMinutes = court(defendant, numberJudges, otherDefendants);
 
 	document.querySelector(
 		'#court-result'
-	).textContent = `${plaintiff}'s hearing will be over in ${hearingConclusionTimeMinutes} minutes.`;
+	).textContent = `${defendant}'s hearing will be over in ${hearingConclusionTimeMinutes} minutes.`;
 
 	const end = performance.now();
 	console.log(`court() execution time: ${(end - start).toFixed(4)} ms`);
